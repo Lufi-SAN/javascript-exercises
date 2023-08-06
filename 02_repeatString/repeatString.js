010b835f3a23e3799d834a3d360efe6ac6d80329
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2023-08-06 18:01:15 +0100

    "Solved repeatString.js"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num == 0 || string == '') {
+        return '';
+    } else if (num < 0) {
+        return "ERROR";
+    } else {
+        string = 'hey';
+        for (let i = 1; i < num; i++) {
+            string = string + 'hey';
+        }
+        return string;
+    } 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+//take string
+//create container for first string
+//expression that adds to the container
