87e5856f0628e9f19b57bab42ffb6a4006fae0d1
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2023-08-07 11:05:10 +0100

    Solved reverseString.js exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringContainer = [];
+    [...stringContainer] = string;
+  	text = "";
+    for(const x of stringContainer.reverse()) {
+    	text += x;  
+      }
+  	return text;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//put in containerz
