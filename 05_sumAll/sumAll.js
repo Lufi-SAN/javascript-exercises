7bfa63b9b26914e1ec9841366b21d1850365530b
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2023-08-09 11:24:21 +0100

    Solved sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,lastNumber) {
+    if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number') return 'ERROR';
+    if (firstNumber < 0 || lastNumber < 0) return 'ERROR';
+  	if ( firstNumber > lastNumber) {
+      let lastN = lastNumber;  
+      lastNumber = firstNumber;
+        firstNumber = lastN;
+        
+    }
+    let middleNumbersContainer = lastNumber;
+    for(let i = lastNumber; i > (firstNumber+1);i--) {
+        lastNumber = lastNumber - 1;
+        middleNumbersContainer += lastNumber;
+    }
+    return middleNumbersContainer + firstNumber;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
