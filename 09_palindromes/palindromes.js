2e3f6f10d1052a22607588a2606e5fed2ceb2869
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2023-08-27 03:50:19 +0100

    Solved palindrome problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arr = string.toLowerCase().replace(/[^a-z0-9]/g,"").split("").reverse().join("").toString();
+    return arr === string.replace(/[^a-z0-9]/g,"").toLowerCase();
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(arr) {
+    arr = Number(arr);
+    let fibarr = [0,1];
+    if(arr < 1) {return ('OOPS')}
+    else if (arr === 1) {return ( fibarr[0] + fibarr[1])}
+    else {
+        let counter = 2;
+        for(let i = 1; i < arr; i++){
+            fibarr.push(fibarr[(counter-2)] + fibarr[(counter-1)]);
+            counter++;
+        }
+        return(fibarr[fibarr.length - 1]);
+    }
 };
 
 // Do not edit below this line
